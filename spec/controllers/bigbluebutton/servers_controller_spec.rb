32fbc7952d15eb32f2e340dc0743cfba4a49b205
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2011-03-22 18:08:43 -0300

    Testing Bigbluebutton::Servers routes and controller.

--- spec/controllers/bigbluebutton/servers_controller_spec.rb
+++ spec/controllers/bigbluebutton/servers_controller_spec.rb
@@ -3,12 +3,44 @@ require 'spec_helper'
 describe Bigbluebutton::ServersController do
 
   render_views
+  let(:server) { Factory.create(:bigbluebutton_server) }
 
   describe "#index" do
     before(:each) { get :index }
     it { should respond_with(:success) }
     it { should assign_to(:servers).with(BigbluebuttonServer.all) }
   end
+
+  describe "#show" do
+    before(:each) { get :show, :id => server.to_param }
+    it { should respond_with(:success) }
+    it { should assign_to(:server).with(server) }
+  end
+
+  describe "#new" do
+    before(:each) { get :new }
+    it { should respond_with(:success) }
+    it { should assign_to(:server).with_kind_of(BigbluebuttonServer) }
+  end
+
+  describe "#edit" do
+    before(:each) { get :edit, :id => server.to_param }
+    it { should respond_with(:success) }
+    it { should assign_to(:server).with(server) }
+  end
+
+  describe "#create" do
+    before :each do
+      expect {
+        post :create, :bigbluebutton_server => Factory.attributes_for(:bigbluebutton_server)
+      }.to change{ BigbluebuttonServer.count }.by(1)
+    end
+    it { should respond_with(:redirect) }
+    it { should redirect_to(bigbluebutton_server_path(BigbluebuttonServer.last)) }
+  end
+
+  it "#update"
+  it "#destroy"
 
 end
 
--- spec/routing/bigbluebutton/servers_routing_spec.rb
+++ spec/routing/bigbluebutton/servers_routing_spec.rb
@@ -5,8 +5,13 @@ describe Bigbluebutton::ServersController do
 
   describe "routing" do
     it { should route(:get, "/bigbluebutton/servers").to(:action => :index) }
+    it { should route(:post, "/bigbluebutton/servers").to(:action => :create) }
+    it { should route(:get, "/bigbluebutton/servers/new").to(:action => :new) }
+    it { should route(:get, "/bigbluebutton/servers/1/edit").to(:action => :edit, :id => 1) }
+    it { should route(:get, "/bigbluebutton/servers/1").to(:action => :show, :id => 1) }
+    it { should route(:put, "/bigbluebutton/servers/1").to(:action => :update, :id => 1) }
+    it { should route(:delete, "/bigbluebutton/servers/1").to(:action => :destroy, :id => 1) }
   end
-  # TODO test all the default routes for 'servers'
 
 end
 
