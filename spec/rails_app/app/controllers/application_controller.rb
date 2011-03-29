bf18607ce10a7e88c488daf984d67a8739a27764
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2011-03-29 17:49:05 -0300

    Small adjustments in the views and in the spec/rails_app.

--- spec/rails_app/app/controllers/application_controller.rb
+++ spec/rails_app/app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+#  def bigbluebutton_user
+#    User.new
+#  end
+
+end
+
+class User
+  attr_accessor :name
+
+  def initialize
+    self.name = "Chuck"
+  end
 end
