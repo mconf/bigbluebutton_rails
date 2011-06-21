996f20464905001f77a958981e6f310b1a4c95ef
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2011-06-21 12:43:13 -0300

    Fixing the paths in the generator (wasn't working when the gem was installed from git)..

--- lib/generators/bigbluebutton_rails/install_generator.rb
+++ lib/generators/bigbluebutton_rails/install_generator.rb
@@ -6,19 +6,19 @@ module BigbluebuttonRails
       include Rails::Generators::Migration
       argument :migrate_to_version, :type => :string, :default => "", :description => "Generate migration to this version"
       class_option :migration_only, :type => :boolean, :default => false, :description => "Generate only the migration file"
-      source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path("../../../../", __FILE__) # set to gem root
 
       desc "Creates the migrations and locale files. Also used to create migrations when updating the gem version."
 
       def copy_locale
-        copy_file "#{root_path}/config/locales/en.yml", "config/locales/bigbluebutton_rails.en.yml" unless options.migration_only?
+        copy_file "config/locales/en.yml", "config/locales/bigbluebutton_rails.en.yml" unless options.migration_only?
       end
 
       def copy_public_files
         unless options.migration_only?
-          copy_file "#{root_path}/public/javascripts/jquery.min.js", "public/javascripts/jquery.min.js"
-          copy_file "#{root_path}/public/images/loading.gif", "public/images/loading.gif"
-          copy_file "#{root_path}/public/stylesheets/bigbluebutton_rails.css", "public/stylesheets/bigbluebutton_rails.css"
+          copy_file "public/javascripts/jquery.min.js", "public/javascripts/jquery.min.js"
+          copy_file "public/images/loading.gif", "public/images/loading.gif"
+          copy_file "public/stylesheets/bigbluebutton_rails.css", "public/stylesheets/bigbluebutton_rails.css"
         end
       end
 
@@ -28,17 +28,16 @@ module BigbluebuttonRails
 
       def create_migration_file
         if migrate_to_version.blank?
-          migration_template 'migration.rb', 'db/migrate/create_bigbluebutton_rails.rb'
+          migration_template '#{migration_path}/migration.rb', 'db/migrate/create_bigbluebutton_rails.rb'
         else
-          migration_template "migration_#{version_filename}.rb", "db/migrate/bigbluebutton_rails_to_#{version_filename}.rb"
+          migration_template "#{migration_path}/migration_#{version_filename}.rb", "db/migrate/bigbluebutton_rails_to_#{version_filename}.rb"
         end
       end
 
       protected
 
-      # defines a fullpath to avoid using the local application files
-      def root_path
-        File.expand_path("../../../../../bigbluebutton_rails", __FILE__)
+      def migration_path
+        File.join("lib", "generators", "bigbluebutton_rails", "templates")
       end
 
       def version_filename
