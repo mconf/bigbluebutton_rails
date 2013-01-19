26519dfecc7eef2ee2b0a45968fe6e5b2198cbd0
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-01-19 20:54:46 -0200

    Remove assets (jquery, image) from the generator and from the gem
    Only a css is now provided as additional asset and generated with bigbluebutton_rails:views.

--- lib/generators/bigbluebutton_rails/install_generator.rb
+++ lib/generators/bigbluebutton_rails/install_generator.rb
@@ -14,14 +14,6 @@ module BigbluebuttonRails
         copy_file "config/locales/en.yml", "config/locales/bigbluebutton_rails.en.yml" unless options.migration_only?
       end
 
-      def copy_public_files
-        unless options.migration_only?
-          copy_file "public/javascripts/jquery.min.js", "public/javascripts/jquery.min.js"
-          copy_file "public/images/loading.gif", "public/images/loading.gif"
-          copy_file "public/stylesheets/bigbluebutton_rails.css", "public/stylesheets/bigbluebutton_rails.css"
-        end
-      end
-
       def self.next_migration_number(dirname)
         ActiveRecord::Generators::Base.next_migration_number(dirname)
       end
--- lib/generators/bigbluebutton_rails/views_generator.rb
+++ lib/generators/bigbluebutton_rails/views_generator.rb
@@ -3,14 +3,18 @@ require 'rails/generators/active_record'
 module BigbluebuttonRails
   module Generators
     class ViewsGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../../../app/views", __FILE__)
+      source_root File.expand_path("../../../../app", __FILE__)
       desc "Copies all bigbluebutton_rails views into your application folders."
 
       argument :scope, :required => false, :default => nil,
                        :desc => "The scope to copy views to"
 
       def copy_views
-        directory "bigbluebutton", "app/views/#{ scope || :bigbluebutton}"
+        directory "views/bigbluebutton", "app/views/#{ scope || :bigbluebutton}"
+      end
+
+      def copy_stylesheets
+        copy_file "assets/stylesheets/bigbluebutton_rails.css", "app/assets/stylesheets/bigbluebutton_rails.css"
       end
 
     end
