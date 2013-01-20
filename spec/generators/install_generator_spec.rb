9b37bf384b89e906d941cadc2158970fe31bd2e6
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-01-20 15:40:24 -0200

    Fix the test for the generators

--- spec/generators/install_generator_spec.rb
+++ spec/generators/install_generator_spec.rb
@@ -14,18 +14,12 @@ describe BigbluebuttonRails::Generators::InstallGenerator do
     it "all files are properly created" do
       assert_migration "db/migrate/create_bigbluebutton_rails.rb"
       assert_file "config/locales/bigbluebutton_rails.en.yml"
-      assert_file "public/stylesheets/bigbluebutton_rails.css"
-      assert_file "public/javascripts/jquery.min.js"
-      assert_file "public/images/loading.gif"
     end
 
     it "all files are properly destroyed" do
       run_generator %w(), :behavior => :revoke
       assert_no_migration "db/migrate/create_bigbluebutton_rails.rb"
       assert_no_file "config/locales/bigbluebutton_rails.en.yml"
-      assert_no_file "public/stylesheets/bigbluebutton_rails.css"
-      assert_no_file "public/javascripts/jquery.min.js"
-      assert_no_file "public/images/loading.gif"
     end
   end
 
@@ -38,9 +32,6 @@ describe BigbluebuttonRails::Generators::InstallGenerator do
     it "only the migration is created" do
       assert_migration "db/migrate/create_bigbluebutton_rails.rb"
       assert_no_file "config/locales/bigbluebutton_rails.en.yml"
-      assert_no_file "public/stylesheets/bigbluebutton_rails.css"
-      assert_no_file "public/javascripts/jquery.min.js"
-      assert_no_file "public/images/loading.gif"
     end
   end
 
