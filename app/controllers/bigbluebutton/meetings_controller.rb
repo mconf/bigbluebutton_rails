5da879bff939e82a0eaf92b8b2c467e4bba3dc58
vitoriamr <vmainardirosa@gmail.com>
2018-07-09 17:15:03 -0300

    Improvements in controller and controller spec: 40%

--- app/controllers/bigbluebutton/meetings_controller.rb
+++ app/controllers/bigbluebutton/meetings_controller.rb
@@ -6,6 +6,8 @@ class Bigbluebutton::MeetingsController < ApplicationController
 
   before_filter :find_meeting
 
+  respond_to :html
+
   layout :determine_layout
 
   def determine_layout
@@ -13,10 +15,29 @@ class Bigbluebutton::MeetingsController < ApplicationController
   end
 
   def destroy
-    @meeting.destroy
+    error = false
+    if @meeting.present?
+      begin
+        @meeting.room.fetch_is_running?
+        @meeting.room.send_end if @meeting.room.is_running?
+        message = t('bigbluebutton_rails.meetings.delete.success')
+      rescue BigBlueButton::BigBlueButtonException => e
+        error = true
+        message = t('bigbluebutton_rails.meetings.notice.destroy.success_with_bbb_error', :error => e.to_s[0..200])
+      end
 
-    message = t('bigbluebutton_rails.meetings.delete.success')
-    redirect_to_using_params_or_back(request.referer, :notice => message)
+      @meeting.destroy
+
+      respond_with do |format|
+        format.html {
+          flash[:error] = message if error
+          redirect_to_using_params_or_back(request.referer, :notice => message)
+        }
+      end
+    else
+      message = t('bigbluebutton_rails.meetings.delete.success')
+      redirect_to_using_params_or_back(request.referer, :notice => message)
+    end
   end
 
   def find_meeting
