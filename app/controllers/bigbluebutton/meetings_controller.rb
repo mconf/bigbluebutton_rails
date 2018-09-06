6c9001a46fa06ae90cbd0b5c986ee1b0c8f3c9fa
Leonardo Eich <leo.eich@hotmail.com>
2018-09-06 13:35:49 -0300

    New meetings controller with new destroy function

--- app/controllers/bigbluebutton/meetings_controller.rb
+++ app/controllers/bigbluebutton/meetings_controller.rb
@@ -15,28 +15,21 @@ class Bigbluebutton::MeetingsController < ApplicationController
   end
 
   def destroy
-    error = false
-    if @meeting.present?
-      begin
-        @meeting.room.fetch_is_running?
-        @meeting.room.send_end if @meeting.room.is_running?
-        message = t('bigbluebutton_rails.meetings.delete.success')
-      rescue BigBlueButton::BigBlueButtonException => e
-        error = true
-        message = t('bigbluebutton_rails.meetings.notice.destroy.success_with_bbb_error', :error => e.to_s[0..200])
-      end
-
-      @meeting.destroy
-
-      respond_with do |format|
-        format.html {
-          flash[:error] = message if error
-          redirect_to_using_params_or_back(request.referer, :notice => message)
-        }
+    if @meeting.ended?
+      if @meeting.destroy
+        respond_with do |format|
+          format.html {
+            flash[:success] = t('bigbluebutton_rails.meetings.delete.success')
+            redirect_to_using_params_or_back(request.referer)
+          }
+        end
+      else
+        flash[:error] = t('bigbluebutton_rails.meetings.notice.destroy.error_destroy')
+        redirect_to_using_params_or_back(request.referer)
       end
     else
-      message = t('bigbluebutton_rails.meetings.delete.success')
-      redirect_to_using_params_or_back(request.referer, :notice => message)
+      flash[:error] = t('bigbluebutton_rails.meetings.notice.destroy.running.not_ended')
+      redirect_to_using_params_or_back(request.referer)
     end
   end
 
