d0188a6990e3e7c8a4984c0a4d99108750fcb2a4
Lucas Flores <lucasflores77@gmail.com>
2018-10-01 11:55:19 -0300

    migration to move recording.description to meeting.title

--- lib/generators/bigbluebutton_rails/templates/migration_2_4_0.rb
+++ lib/generators/bigbluebutton_rails/templates/migration_2_4_0.rb
@@ -1,23 +1,16 @@
 class BigbluebuttonRailsTo240 < ActiveRecord::Migration
   def self.up
-    add_column :bigbluebutton_meetings, :description, :string
     add_column :bigbluebutton_meetings, :title, :string, limit: 80
 
     BigbluebuttonMeeting.find_each do |meeting|
-      if !meeting.name.blank?
-        meeting.update_attributes(title: meeting.name)
-      end
-
       if meeting.name.recorded?
-        meeting.update_attributes(description: meeting.recording.description)
+        meeting.update_attributes(title: meeting.recording.description)
       end
     end
 
-    remove_column :bigbluebutton_meetings, :name
     remove_column :bigbluebutton_recordings, :description
   end
 
   def self.down
   end
-
 end
