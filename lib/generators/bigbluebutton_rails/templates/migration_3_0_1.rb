50822954812a8add8fe1dabf28e7462392bd81dc
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2020-03-29 15:04:57 -0300

    Fix migration to 3.0.1, no need to create all indexes again
    Only to recreate the one that had a column name changed.

--- lib/generators/bigbluebutton_rails/templates/migration_3_0_1.rb
+++ lib/generators/bigbluebutton_rails/templates/migration_3_0_1.rb
@@ -1,7 +1,7 @@
 class BigbluebuttonRailsTo301 < ActiveRecord::Migration
   def self.change
-    add_index :bigbluebutton_meetings, [:room_id, :create_time], using: 'btree'
+    remove_index :bigbluebutton_rooms, [:param], using: 'btree',
+              name: 'bigbluebutton_rooms_param_IDX'
     add_index :bigbluebutton_rooms, [:slug], using: 'btree'
-    add_index :bigbluebutton_metadata, [:owner_id, :owner_type, :name], using: 'btree'
   end
 end
