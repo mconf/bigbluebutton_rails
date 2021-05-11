7b6ae8ed45c704253bce64bc22ab99988d5370da
Marcos Kintschner <marcos.ktn@gmail.com>
2021-05-11 19:01:07 -0300

    Disable BigbluebuttonRecordingsForWorker when using webhooks

--- spec/workers/bigbluebutton_recordings_for_room_worker_spec.rb
+++ spec/workers/bigbluebutton_recordings_for_room_worker_spec.rb
@@ -7,43 +7,59 @@ describe BigbluebuttonRecordingsForRoomWorker do
   end
 
   describe "#perform" do
-    let!(:room) { FactoryGirl.create(:bigbluebutton_room) }
-
-    context "calls #fetch_recordings on the room" do
-      before {
-        BigbluebuttonRoom.stub(:find).and_return(room)
-        expect(room).to receive(:fetch_recordings).once
-      }
+    context 'when using webhooks' do
+      let!(:room) { FactoryGirl.create(:bigbluebutton_room) }
+      before do
+        allow(BigbluebuttonRails.configuration)
+          .to receive(:use_webhooks).and_return(true)
+        allow(BigbluebuttonRoom).to receive(:find).and_return(room)
+        expect(Resque).not_to receive(:enqueue_in)
+      end
       it { BigbluebuttonRecordingsForRoomWorker.perform(room.id) }
     end
 
-    context "if there are still tries left" do
-      before {
-        BigbluebuttonRoom.stub(:find).and_return(room)
-        room.stub(:fetch_recordings)
-        expect(Resque).to receive(:enqueue_in)
-                           .with(5.minutes, ::BigbluebuttonRecordingsForRoomWorker, room.id, 0)
-                           .once
-      }
-      it { BigbluebuttonRecordingsForRoomWorker.perform(room.id, 1) }
-    end
+    context 'when not using webhooks' do
+      let!(:room) { FactoryGirl.create(:bigbluebutton_room) }
+      before do
+        allow(BigbluebuttonRails.configuration)
+          .to receive(:use_webhooks).and_return(false)
+      end
+      context "calls #fetch_recordings on the room" do
+        before {
+          BigbluebuttonRoom.stub(:find).and_return(room)
+          expect(room).to receive(:fetch_recordings).once
+        }
+        it { BigbluebuttonRecordingsForRoomWorker.perform(room.id) }
+      end
 
-    context "if there are no more tries left" do
-      before {
-        BigbluebuttonRoom.stub(:find).and_return(room)
-        room.stub(:fetch_recordings)
-        expect(Resque).not_to receive(:enqueue_in)
-      }
-      it { BigbluebuttonRecordingsForRoomWorker.perform(room.id, 0) }
-    end
+      context "if there are still tries left" do
+        before {
+          BigbluebuttonRoom.stub(:find).and_return(room)
+          room.stub(:fetch_recordings)
+          expect(Resque).to receive(:enqueue_in)
+                            .with(5.minutes, ::BigbluebuttonRecordingsForRoomWorker, room.id, 0)
+                            .once
+        }
+        it { BigbluebuttonRecordingsForRoomWorker.perform(room.id, 1) }
+      end
 
-    context "if the room id is not found" do
-      before {
-        BigbluebuttonRoom.stub(:find).and_return(nil)
-        expect(room).not_to receive(:fetch_recordings)
-        expect(Resque).not_to receive(:enqueue_in)
-      }
-      it { BigbluebuttonRecordingsForRoomWorker.perform(room.id) }
+      context "if there are no more tries left" do
+        before {
+          BigbluebuttonRoom.stub(:find).and_return(room)
+          room.stub(:fetch_recordings)
+          expect(Resque).not_to receive(:enqueue_in)
+        }
+        it { BigbluebuttonRecordingsForRoomWorker.perform(room.id, 0) }
+      end
+
+      context "if the room id is not found" do
+        before {
+          BigbluebuttonRoom.stub(:find).and_return(nil)
+          expect(room).not_to receive(:fetch_recordings)
+          expect(Resque).not_to receive(:enqueue_in)
+        }
+        it { BigbluebuttonRecordingsForRoomWorker.perform(room.id) }
+      end
     end
   end
 end
